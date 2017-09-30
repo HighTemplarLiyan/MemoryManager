fcab33541c89b47b0db8cef6eeeae68113ebb173
Max <max.gavr@tut.by>
2017-09-30 19:02:31 +0300

    Implement First Fit memory management algo with linked list of segments

--- mmemory.c
+++ mmemory.c
@@ -12,12 +12,14 @@
 #include <string.h>
 #include "mmemory.h"
 
-// struct definitions
+///////////////////////////////////////////////////////////////////////////////
+// Struct definitions
+///////////////////////////////////////////////////////////////////////////////
 
 struct Segment
 {
-	VA vaStartAddress;
 	long nSize;
+	bool bIsFree;
 	struct Segment* pNextSegment;
 
 };
@@ -26,7 +28,7 @@ typedef struct Segment Segment;
 typedef struct
 {
 	Segment segment;
-	void* pAddress;
+	void* paAddress;
 	bool bIsPresent;
 
 } SegmentRecord;
@@ -36,76 +38,147 @@ typedef struct
 	SegmentRecord* pFirstRecord;
 	int nSize;
 
-	//Segment* pSegmentListHead;
+	Segment* pSegmentListHead;
 
 } SegmentTable;
 
 ///////////////////////////////////////////////////////////////////////////////
-
-#define VAS_SIZE (1000 * 1024 * 1024) // MB
-#define SEG_TABLE_SIZE(MAX_SEGMENTS) (sizeof(SegmentTable) + sizeof(SegmentRecord) * MAX_SEGMENTS)
+#define LONG(x) ((long)(x))
 
 // number of bytes in VA reserved for segment index and offset
 #define SEG_INDEX_BYTES 2
 #define SEG_OFFSET_BYTES (sizeof(VA) - SEG_INDEX_BYTES)
 
-#define GET_VA_SEG_INDEX(va)          (va >> (8 * SEG_OFFSET_BYTES))
-#define GET_VA_SEG_OFFSET(va)         (va & ((1L << (8 * SEG_OFFSET_BYTES)) - 1))
+#define GET_VA_SEG_INDEX(va)          (LONG(va) >> (8 * SEG_OFFSET_BYTES))
+#define GET_VA_SEG_OFFSET(va)         (LONG(va) & ((1L << (8 * SEG_OFFSET_BYTES)) - 1))
 
-#define SET_VA_SEG_INDEX(va, index)   (va = GET_VA_SEG_OFFSET(va) | (index << (8 * SEG_OFFSET_BYTES)))
-#define SET_VA_SEG_OFFSET(va, offset) (va = ((GET_VA_SEG_INDEX(va) << (8 * SEG_OFFSET_BYTES)) | offset))
+#define SET_VA_SEG_INDEX(va, index)   (va = (VA)(GET_VA_SEG_OFFSET(va) | (index << (8 * SEG_OFFSET_BYTES))))
+#define SET_VA_SEG_OFFSET(va, offset) (va = (VA)(((GET_VA_SEG_INDEX(va) << (8 * SEG_OFFSET_BYTES)) | offset)))
 
 // handles signed negative integers
 // #define GET_VA_SEG_INDEX(va) ((va >> (8 * SEG_OFFSET_BYTES)) & ((0xffL << ((SEG_INDEX_BYTES + 1)*8)) - 1)) 
 
-// globals
+#define VAS_SIZE (1000 * 1024 * 1024) // bytes
+#define MAX_SEGMENTS ((2 << (8*SEG_INDEX_BYTES)) - 1)
+#define SEG_TABLE_SIZE(MAX_SEGMENTS) (sizeof(SegmentTable) + (sizeof(SegmentRecord) * MAX_SEGMENTS))
+
+///////////////////////////////////////////////////////////////////////////////
+// Globals
+///////////////////////////////////////////////////////////////////////////////
 
 void* g_paStartAddress;
 
-VA g_vaFirstFree = NULL;
-const VA g_vaLastAvailable = NULL + VAS_SIZE / sizeof(VA) - 1;
+size_t g_nCurrentVasSize = 0;
 
 SegmentTable* g_pSegmentTable;
 int g_nMaxSegments;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-SegmentRecord* insert_new_record_into_table(VA vaSegmentAddress, size_t nSegmentSize)
+Segment* initialize_free_segment(void* paStartAddress, size_t nSize, Segment* pNextSegment)
 {
+	Segment freeSegment;
+	freeSegment.nSize = nSize - sizeof(Segment);
+	freeSegment.bIsFree = true;
+	freeSegment.pNextSegment = pNextSegment;
+
+	memcpy(paStartAddress, (void*)&freeSegment, sizeof(Segment));
+
+	return (Segment*)paStartAddress;
+}
+
+bool place_segment_into_memory(SegmentRecord* pRecord)
+{
+	Segment* pSegmentToPlace = &(pRecord->segment);
+
+	Segment* pSegment = g_pSegmentTable->pSegmentListHead;
+	Segment* pPreviousSegment = NULL;
+	Segment* pNextSegment = NULL;
+
+	while (pSegment)
+	{
+		pNextSegment = pSegment->pNextSegment;
+
+		// TODO: choose memory management algorithm: best fit, first fit, etc.
+		if (pSegment->bIsFree && pSegment->nSize >= pSegmentToPlace->nSize)
+		{
+			// initialize free segment in the rest of the memory
+			size_t nMemoryLeft = pSegment->nSize - pSegmentToPlace->nSize;
+			if (nMemoryLeft > 0)
+			{
+				void* paNextSegment = (void*)((char*)(pSegment + 1) + pSegment->nSize);
+
+				Segment* pNewFreeSegment = initialize_free_segment(paNextSegment, nMemoryLeft, pNextSegment);
+				pSegmentToPlace->pNextSegment = pNewFreeSegment;
+			}
+			else
+				pSegmentToPlace->pNextSegment = pNextSegment;
+
+			// clear previous free segment record
+			//memset((void*)pSegment, 0, sizeof(Segment));
+
+			if (pPreviousSegment)
+				pPreviousSegment->pNextSegment = pSegmentToPlace;
+			else
+				// if segment is inserted into the beggining of memory
+				g_pSegmentTable->pSegmentListHead = pSegmentToPlace;
+
+			// copy segment content into memory
+			// TODO: bIsPresent assert
+			// TODO: move to separate function
+			if (pRecord->paAddress)
+				memcpy((void*)pSegment, pRecord->paAddress, pSegmentToPlace->nSize);
+			else
+				memset((void*)pSegment, 0, pSegmentToPlace->nSize);
+			pRecord->paAddress = (void*)pSegment;
+			pRecord->bIsPresent = true;
+			
+			return true;
+		}
+
+		pPreviousSegment = pSegment;
+		pSegment = pSegment->pNextSegment;
+	}
+
+	return false;
+}
+
+VA insert_new_record_into_table(size_t nSegmentSize)
+{
+	VA vaNewSegmentAddress = 0L;
+
 	size_t nTableSize = g_pSegmentTable->nSize;
+
+	SET_VA_SEG_INDEX(vaNewSegmentAddress, nTableSize);
+	SET_VA_SEG_OFFSET(vaNewSegmentAddress, 0L);
 
 	SegmentRecord* pNewRecord = g_pSegmentTable->pFirstRecord + nTableSize;
 
 	SegmentRecord tmpRecord;
-	tmpRecord.pAddress = NULL;
+	tmpRecord.paAddress = NULL;
 	tmpRecord.bIsPresent = false;
-	tmpRecord.segment.vaStartAddress = vaSegmentAddress;
 	tmpRecord.segment.nSize = nSegmentSize;
 	tmpRecord.segment.pNextSegment = NULL;
 
 	memcpy((void*)pNewRecord, (void*)&tmpRecord, sizeof(SegmentRecord));
 
-	// link previous segment to the new one
-	if (nTableSize > 1)
-		g_pSegmentTable->pFirstRecord[nTableSize - 1].segment.pNextSegment = &(pNewRecord->segment);
-
 	g_pSegmentTable->nSize++;
-	g_vaFirstFree = vaSegmentAddress + nSegmentSize - 1;
 
-	return pNewRecord;
+	return vaNewSegmentAddress;
 }
 
 int m_malloc(VA* ptr, size_t szBlock)
 {
-	if (!szBlock)
+	// TODO: check max block size
+	if (szBlock < 0)
 		return -1;
 
-	VA vaEndAddress = g_vaFirstFree + szBlock;
-
-	if (vaEndAddress > g_vaLastAvailable)
+	if (g_nCurrentVasSize + szBlock > VAS_SIZE)
 		return -2;
 
-	SegmentRecord* pNewRecord = insert_new_record_into_table(g_vaFirstFree, szBlock);
+	*ptr = insert_new_record_into_table(szBlock);
+	place_segment_into_memory(&g_pSegmentTable->pFirstRecord[GET_VA_SEG_INDEX(ptr)]);
+
 	return 0;
 }
 
@@ -126,9 +199,11 @@ int m_write(VA ptr, void* pBuffer, size_t szBuffer)
 
 int m_init(int n, int szPage)
 {
+	// TODO: check max number of segments and their size
 	if (n <= 0 || szPage <= 0)
 		return -1;
 
+	// TODO: redundant?
 	g_nMaxSegments = n;
 	long nTotalMemory = n * szPage;
 
@@ -137,18 +212,15 @@ int m_init(int n, int szPage)
 		return 1;
 
 	g_pSegmentTable = (SegmentTable*)g_paStartAddress;
+	// TODO: void pointer arithmetic is illegal
 	g_paStartAddress = g_paStartAddress + SEG_TABLE_SIZE(g_nMaxSegments);
 
 	// init segment table
 	SegmentTable tmpTable;
 	tmpTable.pFirstRecord = (SegmentRecord*)(g_pSegmentTable + 1);
 	tmpTable.nSize = 0;
+	tmpTable.pSegmentListHead = initialize_free_segment(g_paStartAddress, nTotalMemory, NULL);
 	memcpy((void*)g_pSegmentTable, (void*)&tmpTable, sizeof(SegmentTable));
-
-	g_vaFirstFree = NULL;
-
-	VA ptr;
-	m_malloc(&ptr, 100);
 
 	return 0;
 }
